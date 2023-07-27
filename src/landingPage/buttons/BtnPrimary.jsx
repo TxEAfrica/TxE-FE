b52d7ae892052d5e77e1a9502bfe49d5403dd3b6
iArney <iarnoldraphael@gmail.com>
2023-07-27 05:29:06 -0700

    initial commit

---
+++ src/landingPage/buttons/BtnPrimary.jsx
@@ -0,0 +1,9 @@
+const BtnPrimary = ({btnName}) => {
+    return ( 
+        <div className="p-2 w-full flex justify-center items-center text-center bg-orange-500 rounded-md text-white">
+            {btnName}
+        </div>
+     );
+}
+ 
+export default BtnPrimary;
---
+++ src/landingPage/buttons/BtnSecondary.jsx
@@ -0,0 +1,9 @@
+const BtnSecondary = ({btnName}) => {
+    return ( 
+        <div className="p-2 w-full flex justify-center items-center text-center border-2 border-orange-500 rounded-md md:text-white">
+            {btnName}
+        </div>
+     );
+}
+ 
+export default BtnSecondary;
