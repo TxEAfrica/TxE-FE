a61fec959b0893a6544591e4f0d65c869c2ed3c6
theseyipeters <theseyipeters@gmail.com>
2023-08-03 18:35:51 +0100

    functioning Grant Form

--- src/applications/grant/EmailVerification.js
+++ src/applications/grant/EmailVerification.js
@@ -1,83 +1,92 @@
 // EmailVerification.js
-import React, { useState } from 'react';
-import InputField from '../../registrationPage/eventregistration/components/InputField/InputField';
-import FormBtn from '../../registrationPage/eventregistration/components/Buttons/FormButton';
-// import ButtonsCSS from './components/Buttons/ButtonsCSS.module.css'
-import ApplyForGrantCSS from '../grant/ApplyForGrant.module.css'
-import FailedModal from '../../modals/FailedModal'
-import SuccessModal from '../../modals/SuccessModal';
+import React, { useState } from "react";
+import InputField from "../../registrationPage/eventregistration/components/InputField/InputField";
+import FormBtn from "../../registrationPage/eventregistration/components/Buttons/FormButton";
+import ApplyForGrantCSS from "../grant/ApplyForGrant.module.css";
+import FailedModal from "../../modals/FailedModal";
+import { useNavigate, Navigate } from "react-router-dom";
 
 const EmailVerification = ({ onSuccess, onUserData }) => {
-  const [email, setEmail] = useState('');
-  const [isEmailVerified, setIsEmailVerified] = useState(false);
-  const [verificationFailed, setVerificationFailed] = useState(false);
-  const [showSuccessModal, setShowSuccessModal] = useState(false);
-  const [showFailedModal, setShowFailedModal] = useState(false);
+	const [email, setEmail] = useState("");
+	const [verificationFailed, setVerificationFailed] = useState(false);
+	const [showFailedModal, setShowFailedModal] = useState(false);
+	const [failedModalMessage, setFailedModalMessage] = useState("");
+	const [failedSecondModalMessage, setFailedSecondModalMessage] = useState("");
+	const { buttonMessage, setButtonMessage } = useState("");
+	// const { handleNavigate, setHandleNavigate } = useState({});
 
-  const [userData, setUserData] = useState([])
+	const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
-  const handleVerifyEmail = async (e) => {
-    e.preventDefault();
+	const handleVerifyEmail = (e) => {
+		e.preventDefault();
+		if (!emailRegex.test(email)) {
+			setVerificationFailed(true);
+			setFailedModalMessage("Please enter a valid email address");
+			return;
+		}
 
-    try {
+		fetch(`https://txe-africa.onrender.com/api/v1/${email}`)
+			.then((response) => {
+				return response.json();
+			})
+			.then((data) => {
+				if (data && data.data.trackInterest === "entrepreneurship") {
+					setVerificationFailed(false);
+					onSuccess(data, true); // Pass the user data and verification status back to the parent component
+				} else {
+					setVerificationFailed(true);
+					setFailedModalMessage("You are ineligible to apply for a Grant");
+					setFailedSecondModalMessage(
+						"This grant is only available to Entrepreneur Applicants"
+					);
+					setButtonMessage("Click here to apply for Tech Support");
+					// setHandleNavigate(() => Navigate("/"));
+					onSuccess(null, false); // Pass null as user data and verification status back to the parent component
+				}
+			})
+			.catch((error) => {
+				setVerificationFailed(true);
+				// setFailedModalMessage("You have not registered for the event");
+				// setFailedSecondModalMessage(
+				// 	"Kindly Register for TxE Summit 2023"
+				// );
+				onSuccess(null, false); // Pass null as user data and verification status back to the parent component
+			});
+	};
 
+	return (
+		<div className={ApplyForGrantCSS.mainn}>
+			<form>
+				<InputField
+					labelText="Email Address"
+					placeholder="Verify email address"
+					htmlFor="email"
+					inputId="email"
+					type="email"
+					value={email}
+					onChange={(e) => setEmail(e.target.value)}
+				/>
 
-      const response = await fetch(`https://txe-africa.onrender.com/api/v1/${email}`);
+				<div>
+					<FormBtn
+						btnFor="Next"
+						onClick={handleVerifyEmail}
+					/>
+				</div>
+			</form>
 
-      if (response.ok) {
-        setIsEmailVerified(true);
-        setVerificationFailed(false);
-        const data = await response.json();
-
-        // Pass the data to the parent component using the onUserData callback
-        if (onUserData) {
-          onUserData(data);
-        }
-
-        if (onSuccess) {
-          onSuccess();
-          // console.log(data)
-        }
-      } else {
-        setIsEmailVerified(false);
-        setVerificationFailed(true);
-      }
-    } catch (error) {
-      console.error('Error verifying email:', error);
-      setIsEmailVerified(false);
-      setVerificationFailed(true);
-    }
-  };
-
-  return (
-    <div className={ApplyForGrantCSS.mainn}>
-
-      <form>
-        <InputField
-            labelText="Email Address"
-            placeholder={"Verify email address"}
-            htmlFor="email" 
-            inputId="email" 
-            type="email" 
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-              
-        <div>
-          <FormBtn btnFor={"Next"} onClick={handleVerifyEmail} />
-        </div>
-      </form>
-        {/* {verificationFailed && 
-        <div>
-          <FailedModal
-            onClose={() => setShowFailedModal(false)}
-            message={"You need to register for the event"}
-            secondMessage={"Then you can come back and apply for a Grant"}  
-          />
-        </div>} */}
-
-    </div>
-  );
+			{verificationFailed && (
+				<div>
+					<FailedModal
+						onClose={() => window.location.reload()}
+						message={failedModalMessage}
+						secondMessage={failedSecondModalMessage}
+						btnFor={buttonMessage}
+					/>
+				</div>
+			)}
+		</div>
+	);
 };
 
 export default EmailVerification;
